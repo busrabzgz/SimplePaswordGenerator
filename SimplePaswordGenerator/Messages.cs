1604d72d780d3d8300f52f6107126a908549e37c
busrabozgoz <bozgozbusra@gmail.com>
2023-05-10 16:55:57 +0300

    Add project files.

---
+++ SimplePaswordGenerator/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace SimplePaswordGenerator;
+
+public static class Questions
+{
+    public static string IncludeNumbers => "Do you want to include numbers?";
+    public static string IncludeLowerCaseCharacters => "Ok! How about lowercase characters?";
+    public static string IncludeUpperCaseCharacters => "very nice! How about uppercase characters?";
+
+    public static string IncludeSpecialCharacters =>
+        "All right! We are almost done. Would you also want to add special characters? ";
+
+    public static string PasswordLength => "Great! Lastly . How long do you want to keep your password length?";
+}
---
+++ SimplePaswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        private const string Numbers = "0123456789";
+        private const string LowercaseCharacters = "abcdefghijklmnoprstuvyzwxq";
+
+        private const string UppercaseCharacters = "ABCDEFGHIJKLMNOPRSTUVYZXWQ";
+        private const string SpecialCharacters = "!@#$^()?";
+        private int _length = 0;
+
+        private bool _isUserInputValid=true;
+
+        
+
+
+    }
+}
+
+
