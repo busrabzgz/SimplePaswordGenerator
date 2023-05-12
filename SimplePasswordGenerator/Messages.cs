2d748f7cc5fd7f80a7fd5afce26232f8edf5ff6e
Büşra <98875836+busrabzgz@users.noreply.github.com>
2023-05-12 16:40:03 +0300

    completed
    completed simple passwordgeneraator

---
+++ SimplePasswordGenerator/Messages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SimplePasswordGenerator
+{
+	public static class Messages
+	{
+
+
+        public static class Questions
+        {
+            public static string IncludeNumbers => "Do you want to include numbers?";
+            public static string IncludeLowerCaseCharacters => "Ok! How about lowercase characters?";
+            public static string IncludeUpperCaseCharacters => "very nice! How about uppercase characters?";
+
+            public static string IncludeSpecialCharacters =>
+                "All right! We are almost done. Would you also want to add special characters? ";
+
+            public static string PasswordLength => "Great! Lastly . How long do you want to keep your password length?";
+        }
+           
+		
+
+
+        
+
+
+    }
+}
+
---
+++ SimplePasswordGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimplePasswordGenerator;
+
+var passwordGenerator = new PasswordGenerator();
+passwordGenerator.Greetings();
+passwordGenerator.ReadInputs();
+passwordGenerator.Generate();
+passwordGenerator.WriteGeneratedPasswors();
+Console.ReadLine();
+return 0;
+
